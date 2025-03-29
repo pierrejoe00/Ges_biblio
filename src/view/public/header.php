2c593b3caa0e21af277910eec77e9cad3e530ae1
pierrejo00 <piejoe003@gmail.com>
2025-03-29 05:36:46 +0000

    Remove unused image files and update navbar styles and structure for improved navigation

--- src/view/public/header.php
+++ src/view/public/header.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    
 </head>
 <body>
--- src/view/public/navbar.php
+++ src/view/public/navbar.php
@@ -1,16 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">IPP GESTION</a>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #7f2b18;">
+  <style>
+    .navbar-brand {
+      color:rgb(8, 3, 3) !important;
+    }
+    .navbar-nav .nav-link:not(.active) {
+      font-family: 'Arial', sans-serif;
+      color: rgb(247, 246, 243) !important;
+    }
+  </style>
+  <a class="navbar-brand" href="#">COJO's LIBRAIRIE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-    <a class="nav-item nav-link active" href="?action=listLivre">Liste des livres</a>
-    <a class="nav-item nav-link active" href="?action=listCategorie">Liste des categories</a>
-      <a class="nav-item nav-link active" href="?action=listAuteur">Liste des auteurs</a>
-      <a class="nav-item nav-link " href="?action=listUsers">Liste des users</a>
-      <a class="nav-item nav-link" href="?action=listRole">Liste des rôles</a>
-      
+    <div class="navbar-nav mx-auto">
+      <a class="nav-item nav-link active" href="?action=listLivre">Gestion des livres</a>
+      <br>
+      <a class="nav-item nav-link " href="?action=listCategorie">Gestion des categories</a>
+      <br>
+      <a class="nav-item nav-link " href="?action=listAuteur">Gestion des auteurs</a>
+      <br>
+      <a class="nav-item nav-link " href="?action=listUsers">Gestion des users</a>
+      <br>
+      <a class="nav-item nav-link " href="?action=listRole">Gestion des rôles</a>
+    </div>
+    <div class="navbar-nav ml-auto d-flex justify-content-end">
+      <span class="nav-item nav-link">
+        <?php echo $_SESSION['prenom'] ?? 'Utilisateur'; ?>
+      </span>
+      <a class="nav-item nav-link" href="?action=logout">Déconnexion</a>
     </div>
   </div>
 </nav>
